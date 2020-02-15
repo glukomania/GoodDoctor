17909bba8d77eeab6847fa162ab2cfc00ede5782
glukomania <glukomani@gmail.com>
2020-02-15 23:27:20 +0100

    First start, first page

---
+++ gooddoctor/src/Main/main.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./main.css";
+
+const Main = () => {
+  return (
+    <div className="main-container">
+      <div className="header">
+        <div className="logo">
+          <div>
+            <a href="3">GoodDoctor</a>
+          </div>
+          <div className="logo-slogan">Найди своего доктора в Праге</div>
+        </div>
+        <div className="addNew">
+          <div className="AddNew-icon">
+            <a href="#">
+              <i className="fa fa-plus-circle"></i>
+            </a>
+          </div>
+          <div className="addNew-text">
+            <a href="#">Добавить нового доктора</a>
+          </div>
+        </div>
+      </div>
+      <div className="search">
+        <div className="search-wrapper">
+          <div className="ageToggle-wrapper">
+            <div className="adult">
+              <div className="toggle">Взрослый</div>
+              {/* <button className="toggle_btn">Взрослый</button> */}
+            </div>
+            <div className="kids">
+              <div className="toggle disabled">Детский</div>
+              {/* <button className="toggle_btn disabled">Детский</button> */}
+            </div>
+          </div>
+        </div>
+        <div className="search-dropdown__wrapper">
+          <select id="specialization" className="search-dropdown">
+            <option value="volvo">Терапевт</option>
+            <option value="saab">Невролог</option>
+            <option value="opel">Хирург</option>
+            <option value="audi">Аллерголог</option>
+          </select>
+        </div>
+      </div>
+      <div className="search-button">
+        <button type="submit" className="search_btn">
+          Искать
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
